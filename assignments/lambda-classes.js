8b0c9c185bfaa68e9d83ad332e7b41c3994f18c0
SAIPHER ZURETI <saipherzureti@gmail.com>
2019-06-13 17:33:34 -0500

    Update: created base-class and imported code from JavaScript-III

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,16 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(name, age, location){
+        this.name = "Saipher";
+        this.age = 29;
+        this.location = "Chicago";
+        // this.speak = function(){
+        //     console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
+        // }  
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
